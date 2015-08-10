aaa7c3105ddab9af11314b349e3c16e1d2418254
J C Lawrence <claw@kanga.nu>
2015-08-10 16:03:06 -0700

    deps

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 #! /usr/bin/env python
 
-try:
-  import pyver
-except ImportError:
-  import pip
-  pip.main (['install', 'pyver'])
-  import pyver # pylint: disable=W0611
-            
 from setuptools import setup, find_packages
-import glob
-
-__version__, __version_info__ = pyver.get_version (pkg = "qeventlog",
-                                                   public = True)
+import glob, versioneer
 
 setup (
     name = "qeventlog",
-    version = __version__,
+    version = versioneer.get_version (),
     description = "Celery job event logger ",
     long_description = file ("README.rst").read (),
+    cmdclass = versioneer.get_cmdclass (),
     classifiers = [
       "Development Status :: 4 - Beta",
       "Intended Audience :: Developers",
